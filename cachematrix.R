5741c0f1ede457820bc43069af5a28c99c8f4112
vkukrety <vkukrety@gmail.com>
2015-07-26 01:10:22 +0530

    Updates to the functions from vkukrety

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,58 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Comments are sprinkled throughout the code
 
-## Write a short comment describing this function
+## This function creates an object with the cached variables that store the matrix and its inverse as well as the set/get functions
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  # initialize variables for the object
+  minv <- NULL
+  setflag <- 0 # this flag is to check if the matrix has changed
+  
+  # set function sets the value of the matrix
+  set <- function(y) {
+    # set the cached matrix and inverse
+    x <<- y
+    minv <<- NULL
+    setflag <<- 1 # set the flag because matrix has changed and calculated inverse hasn't
+  }
+  get <- function() x # simple function to retrieve the stored matrix
+  getflag <- function() setflag # just a check function, not essential ! 
+  # set the value of the inverted matrix to the cache
+  setinv <- function(solve){
+    minv <<- solve
+    #reset flag, important so that the get function gets value from cache now
+    setflag <<- 0
+  }
+  # get the value from the cache or calculate, store and return 
+  getinv <- function(){
+    dirty <- NULL
+    if(setflag ==0){ # the inverse is same as this matrix
+    }else{ # set was called to set a new matrix
+      dirty <- "T"
+    }
+    # return a list of inverse matrix and the flag
+    list(minv=minv,dirty=dirty)
+  }	
+  # return a list of functions of the object
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv,getflag=getflag)
+  
 }
 
 
-## Write a short comment describing this function
+##Returns a matrix that is the inverse of 'x'
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+  dirty <- NULL
+  inv <- x$getinv() #get the list object
+  if(!is.null(inv$minv) && is.null(inv$dirty)){ # if the flag is not dirty and inverse object is not null then return from cache 
+    message("inverse exists already, getting cached data")
+    return(inv)
+  } # else get matrix, calculate inverse and set its value to the cache
+  mat <- x$get()
+  m <- solve(mat)
+  x$setinv(m)
+  m
+  
 }
